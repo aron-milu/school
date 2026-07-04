1f8c7c89713adcf4c3a400706e98896a81a1067c
Aron Alemseged <aronmilahela@gmail.com>
2026-07-04 16:03:33 +0300

    Test

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -169,7 +169,7 @@ export default function Login() {
                 </div>
               </div>
               <div className="mt-14 max-w-xl space-y-6">
-                <h3 className="text-4xl font-extrabold leading-tight">A community platform connecting schools, students, and parents through online classes and collaborative networks.</h3>
+                <h5 className="text-2xl font-extrabold leading-tight">A community platform connecting schools, students, and parents through online classes and collaborative networks.</h5>
               </div>
               <div className="mt-12 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
                 {FEATURES.map(feature => (
